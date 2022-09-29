12aef78be675a78669f0e2041297cfa4aa7641fe
Arbaz khan <arbaz582021@gmail.com>
2022-09-29 14:14:54 +0500

    add

---
+++ Rxjs/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Rxjs';
+  postArray=[  //this is the array that i am going to convert into obervable
+  //through from operator 
+    {name:"Arbaz khan", id:1},
+    {name:"umar khan", id:1},
+    {name:"Mughaira khan", id:1},
+  ]
+   //is use dollar at the  end because of symbol that
+  //varibale name i am going to convert into an obserable 
+  postArrayAsObervable$= from(this.postArray)
+  //from operator convert postArray into the observable that i will call into
+  // construtor through subscribe method
+  constructor(){
+
+    this.postArrayAsObervable$.subscribe( //into the construtor i am subscribing my observale through subscribe method that take  an object having a three method next error and complete if the program run successfully then next method will be run  and the message will be show Action has performed successfully and if there is any error then error method will be exectued  
+
+      {
+        next:(data)=>console.log(data),
+        error:(error)=>console.log(error),
+        complete:()=>console.log("Action has been performed completely"),
+        
+        
+      }
+    )
+  }
+}
